d101eee676851051280cd25861be66b735d44a90
Marcus Thomas <76545197+marcusdethomas@users.noreply.github.com>
2022-09-22 22:14:17 -0700

    updated server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask_app import app
 from flask_app.controllers import users, topping, pizza
+from flask import Flask
 
+app = Flask(__name__)
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=False)
